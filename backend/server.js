636d21a49c78601c5ea9357c942e02e3b2307316
Berk Soroğlu <59825289+looppin@users.noreply.github.com>
2023-10-19 02:54:14 +0300

    installed mongodb and created folder according to SRP

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 9000;
+
+
+mongoose.connect(process.env.DB_URL);
 
 app.get("/", (req, res) => {
     res.status(200).send("Merhaba!");
